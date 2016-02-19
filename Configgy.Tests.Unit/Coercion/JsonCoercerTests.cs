f0a81d0b498548dffd4ffd95f02c7a7899846f1b
David Love <bungeemonkee@gmail.com>
2016-02-19 16:04:28 -0500

    Adds json and xml coercer tests

---
+++ Configgy.Tests.Unit/Coercion/JsonCoercerTests.cs
@@ -0,0 +1,54 @@
+﻿using Configgy.Coercion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Configgy.Tests.Unit.Coercion
+{
+    [TestClass]
+    public class JsonCoercerTests
+    {
+        [TestMethod]
+        public void JsonCoercer_CoerceTo_Works_With_Array_Of_Int()
+        {
+            const string input = "[1,4,78,222]";
+            var expected = new int[] { 1, 4, 78, 222 };
+
+            var coercer = new JsonCoercer();
+
+            var result = coercer.CoerceTo<int[]>(input, null, null) as int[];
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void JsonCoercer_CoerceTo_Works_With_Dictionary_Of_String_String()
+        {
+            const string input = "{'Banana':'Good','Apple':'Yummy','Radish':'Icky'}";
+            var expected = new Dictionary<string, string>
+            {
+                ["Banana"] = "Good",
+                ["Apple"] = "Yummy",
+                ["Radish"] = "Icky"
+            };
+
+            var coercer = new JsonCoercer();
+
+            var result = coercer.CoerceTo<Dictionary<string, string>>(input, null, null) as IDictionary;
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void JsonCoercer_CoerceTo_Returns_Null_With_Invalid_Json()
+        {
+            const string input = "{";
+
+            var coercer = new JsonCoercer();
+
+            var result = coercer.CoerceTo<Dictionary<string, string>>(input, null, null);
+
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ Configgy.Tests.Unit/Coercion/XmlCoercerTests.cs
@@ -0,0 +1,50 @@
+﻿using Configgy.Coercion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace Configgy.Tests.Unit.Coercion
+{
+    [TestClass]
+    public class XmlCoercerTests
+    {
+        [TestMethod]
+        public void XmlCoercer_CoerceTo_Works_With_Array_Of_Int()
+        {
+            const string input =
+@"<?xml version=""1.0"" encoding=""utf-16""?>
+<ArrayOfInt xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+    <int>1</int>
+    <int>4</int>
+    <int>78</int>
+    <int>222</int>
+</ArrayOfInt>";
+
+            var expected = new int[] { 1, 4, 78, 222 };
+
+            var coercer = new XmlCoercer();
+
+            var result = coercer.CoerceTo<int[]>(input, null, null) as ICollection;
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void XmlCoercer_CoerceTo_Returns_Null_With_Invalid_Xml()
+        {
+            const string input =
+@"<?xml version=""1.0"" encoding=""utf-16""?>
+<ArrayOfInt xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+    <int>1</int
+    <int>4</int>
+    <int>78</int>
+    <int>222</int>
+</ArrayOfInt>";
+
+            var coercer = new XmlCoercer();
+
+            var result = coercer.CoerceTo<int[]>(input, null, null);
+
+            Assert.IsNull(result);
+        }
+    }
+}
