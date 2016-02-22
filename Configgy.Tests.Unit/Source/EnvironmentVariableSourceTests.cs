356e04136eca9605b2335737b2d47af9a7b03924
David Love <bungeemonkee@gmail.com>
2016-02-21 23:13:08 -0500

    Adds missing unit tests for EnvironmentVariableSource

---
+++ Configgy.Tests.Unit/Source/EnvironmentVariableSourceTests.cs
@@ -0,0 +1,48 @@
+﻿using Configgy.Source;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Configgy.Tests.Unit.Source
+{
+    [TestClass]
+    public class EnvironmentVariableSourceTests
+    {
+        public const string Name = "environment variable testing name";
+        public const string Value = "environment variable testing value";
+
+        [TestInitialize]
+        public void Begin()
+        {
+            Environment.SetEnvironmentVariable(Name, Value, EnvironmentVariableTarget.Process);
+        }
+
+        [TestCleanup]
+        public void End()
+        {
+            Environment.SetEnvironmentVariable(Name, null, EnvironmentVariableTarget.Process);
+        }
+
+        [TestMethod]
+        public void GetRawValue_Returns_Value_From_Environment()
+        {
+
+            var source = new EnvironmentVariableSource();
+
+            var result = source.GetRawValue(Name, null);
+
+            Assert.AreEqual(Value, result);
+        }
+
+        [TestMethod]
+        public void GetRawValue_Returns_Null_When_No_Matching_Environment_Variable()
+        {
+            var name = Name + " garbage string: P(*TO(*HFJJJFS#@(**&&^$%#*&()FDGO*^FDC VBNJUYT";
+
+            var source = new EnvironmentVariableSource();
+
+            var result = source.GetRawValue(name, null);
+
+            Assert.IsNull(result);
+        }
+    }
+}
