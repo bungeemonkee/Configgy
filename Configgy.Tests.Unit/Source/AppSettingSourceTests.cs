c0724fadd753b31976b61a3dad05cec97bfbbadc
David Love <bungeemonkee@gmail.com>
2016-02-21 22:48:38 -0500

    Adds missing unit tests for AppSettingSource and ConnectionStringSource

---
+++ Configgy.Tests.Unit/Source/AppSettingSourceTests.cs
@@ -0,0 +1,34 @@
+﻿using Configgy.Source;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Configgy.Tests.Unit.Source
+{
+    [TestClass]
+    public class AppSettingSourceTests
+    {
+        [TestMethod]
+        public void GetRawValue_Returns_String_From_AppSettings()
+        {
+            var name = "name";
+            var value = "value";
+
+            var source = new AppSettingSource();
+
+            var result = source.GetRawValue(name, null);
+
+            Assert.AreEqual(value, result);
+        }
+
+        [TestMethod]
+        public void GetRawValue_Returns_Null_When_No_Matching_AppSettings_Value()
+        {
+            var name = "kfnnpa";
+
+            var source = new AppSettingSource();
+
+            var result = source.GetRawValue(name, null);
+
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ Configgy.Tests.Unit/Source/ConnectionStringSourceTests.cs
@@ -0,0 +1,34 @@
+﻿using Configgy.Source;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Configgy.Tests.Unit.Source
+{
+    [TestClass]
+    public class ConnectionStringSourceTests
+    {
+        [TestMethod]
+        public void GetRawValue_Returns_String_From_ConnectionStrings()
+        {
+            var name = "name";
+            var value = "value";
+
+            var source = new ConectionStringsSource();
+
+            var result = source.GetRawValue(name, null);
+
+            Assert.AreEqual(value, result);
+        }
+
+        [TestMethod]
+        public void GetRawValue_Returns_Null_When_No_Matching_ConnectionStrings_Value()
+        {
+            var name = "kfnnpa";
+
+            var source = new ConectionStringsSource();
+
+            var result = source.GetRawValue(name, null);
+
+            Assert.IsNull(result);
+        }
+    }
+}
