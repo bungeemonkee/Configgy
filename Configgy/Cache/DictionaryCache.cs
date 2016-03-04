2b8fc7354cdbec332b27391e21bd1b598f4245c2
David Love <bungeemonkee@gmail.com>
2016-03-04 12:45:07 -0500

    Simplifies the dictionary cache by using a ConcurrentDictionary<,>

--- Configgy/Cache/DictionaryCache.cs
+++ Configgy/Cache/DictionaryCache.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace Configgy.Cache
 {
@@ -8,52 +8,22 @@ namespace Configgy.Cache
     /// </summary>
     public class DictionaryCache : IValueCache
     {
-        private IDictionary<string, object> _internal = new Dictionary<string, object>();
+        private ConcurrentDictionary<string, object> _internal = new ConcurrentDictionary<string, object>();
 
         public void Clear()
         {
-            lock (this)
-            {
-                _internal.Clear();
-            }
+            _internal.Clear();
         }
 
         public object GetValue(string valueName, Func<string, object> valueCallback)
         {
-            // get the value from the dictionary
-            object result;
-            if (_internal.TryGetValue(valueName, out result))
-            {
-                return result;
-            }
-
-            // lock so only one thread produces and ad the value
-            lock(this)
-            {
-                // make sure no thread added the value while we waited for the lock
-                if (_internal.TryGetValue(valueName, out result))
-                {
-                    return result;
-                }
-
-                // the value doesn't exist - produce it from the callback
-                result = valueCallback(valueName);
-
-                // put the value in the dictionary
-                _internal.Add(valueName, result);
-            }
-
-            return result;
+            return _internal.GetOrAdd(valueName, valueCallback);
         }
 
         public void RemoveValue(string valueName)
         {
-            if (!_internal.ContainsKey(valueName)) return;
-
-            lock (this)
-            {
-                _internal.Remove(valueName);
-            }
+            object value;
+            _internal.TryRemove(valueName, out value);
         }
     }
 }
