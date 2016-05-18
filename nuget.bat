ed0152db207165fd900e22bb3955db4facd0d7a1
David Love <bungeemonkee@gmail.com>
2016-05-18 10:38:39 -0400

    Big improvement to the nuget script

--- nuget.bat
+++ nuget.bat
@@ -1,2 +1,32 @@
-msbuild Configgy.sln /p:Configuration=Release
+@echo off
+
+reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\14.0" /v MSBuildToolsPath > nul 2>&1
+if ERRORLEVEL 1 goto MissingMSBuildRegistry
+
+for /f "skip=2 tokens=2,*" %%A in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\14.0" /v MSBuildToolsPath') do SET MSBUILDDIR=%%B
+
+IF NOT EXIST "%MSBUILDDIR%" goto MissingMSBuildToolsPath
+IF NOT EXIST "%MSBUILDDIR%msbuild.exe" goto MissingMSBuildExe
+
+"%MSBUILDDIR%msbuild.exe" /version
+
+"%MSBUILDDIR%msbuild.exe" Configgy.sln /p:Configuration=Release
 .\nuget.exe pack Configgy\Configgy.csproj -Prop Configuration=Release
+
+:Exit
+pause
+exit
+
+::ERRORS
+::---------------------
+:MissingMSBuildRegistry
+echo Cannot obtain path to MSBuild tools from registry
+goto Exit
+
+:MissingMSBuildToolsPath
+echo The MSBuild tools path from the registry '%MSBUILDDIR%' does not exist
+goto Exit
+
+:MissingMSBuildExe
+echo The MSBuild executable could not be found in '%MSBUILDDIR%'
+goto Exit
