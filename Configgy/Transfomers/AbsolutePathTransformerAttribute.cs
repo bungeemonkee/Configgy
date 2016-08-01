0c62b8dc846a50145975086271f2a13e07b3bb93
David Love <bungeemonkee@gmail.com>
2016-08-01 11:04:42 -0400

    Adds a transformer that converts file paths to the absolute path

---
+++ Configgy/Transfomers/AbsolutePathTransformerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Configgy.Transfomers
+{
+    /// <summary>
+    /// A value transformer that converts a relative file path to a full file path.
+    /// </summary>
+    public class AbsolutePathTransformerAttribute : ValueTransformerAttributeBase
+    {
+        public override string Transform(string value, string valueName, PropertyInfo property)
+        {
+            return value == null
+                ? null
+                : Path.GetFullPath(value);
+        }
+    }
+}
