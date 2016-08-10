1943023bb21546a9d619dd17cb4f05d16f1911b3
David Love <bungeemonkee@gmail.com>
2016-08-10 11:51:55 -0400

    Add uppercase and lowercase transformers
    https://github.com/bungeemonkee/Configgy/issues/24

---
+++ Configgy/Transformation/LowercaseTransformerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace Configgy.Transformation
+{
+    /// <summary>
+    /// A value transformer that makes a string lower case.
+    /// </summary>
+    public class LowercaseTransformerAttribute : ValueTransformerAttributeBase
+    {
+        public CultureInfo Culture { get; set; } = CultureInfo.InvariantCulture;
+
+        public override string Transform(string value, string valueName, PropertyInfo property)
+        {
+            return value?.ToLower(Culture);
+        }
+    }
+}
---
+++ Configgy/Transformation/UppercaseTransformerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace Configgy.Transformation
+{
+    /// <summary>
+    /// A value transformer that makes a string upper case.
+    /// </summary>
+    public class UppercaseTransformerAttribute : ValueTransformerAttributeBase
+    {
+        public CultureInfo Culture { get; set; } = CultureInfo.InvariantCulture;
+
+        public override string Transform(string value, string valueName, PropertyInfo property)
+        {
+            return value?.ToUpper(Culture);
+        }
+    }
+}
