7a6da4301812d52bdbebf3da4315f6fcacf82565
David Love <bungeemonkee@gmail.com>
2016-08-10 12:23:33 -0400

    Increase version to 2.0.2-rc1

--- Configgy/Properties/AssemblyInfo.cs
+++ Configgy/Properties/AssemblyInfo.cs
@@ -24,9 +24,9 @@ using System.Reflection;
 //    Minor Version : Increased for each non-api-breaking release
 //    Build Number : 0 for alpha versions, 1 for beta versions, 2 for release candidates, 3 for releases
 //    Revision : Always 0 for release versions, always 1+ for alpha, beta, rc versions to indicate the alpha/beta/rc number
-[assembly: AssemblyVersion("2.0.1.2")]
-[assembly: AssemblyFileVersion("2.0.1.2")]
+[assembly: AssemblyVersion("2.0.2.1")]
+[assembly: AssemblyFileVersion("2.0.2.1")]
 
 // This version number will roughly follow semantic versioning : http://semver.org
 // The first three numbers will always match the first the numbers of the version above.
-[assembly: AssemblyInformationalVersion("2.0.1-beta2")]
+[assembly: AssemblyInformationalVersion("2.0.2-rc1")]
