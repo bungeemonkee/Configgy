c84bda1cf21bdc62b45bfa8f05ba108bb0cbb0bd
David Love <bungeemonkee@gmail.com>
2017-08-21 12:18:21 -0400

    Replace '_build' directory with submodule 1/2

--- _build/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set scripts=%~dp0
-set scripts=%scripts:~0,-1%
-
-set solution=Configgy.sln
-
-if not defined configuration set configuration=Release
-
-dotnet build -c %configuration% %solution%
--- _build/restore.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set scripts=%~dp0
-set scripts=%scripts:~0,-1%
-
-set solution=Configgy.sln
-
-dotnet restore %solution%
